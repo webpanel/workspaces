e81caaf68acf47502a6f5822f7024925a06f9da3
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2019-09-03 11:40:05 +0200

    Add envvar

---
+++ lib/env.d.ts
@@ -0,0 +1,5 @@
+interface IENV {
+    REACT_APP_API_URL: string;
+}
+export declare const ENV: IENV;
+export {};
--- src/model/api.tsx
+++ src/model/api.tsx
@@ -1,9 +1,10 @@
 import { DataSource, ResponseDataTransformer } from 'webpanel-data';
 
 import { GraphQLORMConnector as Connector } from 'graphql-orm-connetor';
+import { ENV } from '../env';
 
 const connector = new Connector({
   responseDataTransformer: new ResponseDataTransformer()
 });
 
-export const api = new DataSource('api', connector, 'URL');
+export const api = new DataSource('api', connector, ENV.REACT_APP_API_URL);
